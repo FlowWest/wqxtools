bbf03bfb3bb530f08195ad2412c597f9a525bb39
emanuel <erodriguez@gmail.com>
2020-01-22 09:11:22 -0800

    update code to be simple function calls

--- wqxtools/wqx.py
+++ wqxtools/wqx.py
@@ -1,21 +1,33 @@
+import json
 import requests
 import base64
 import hmac
 from datetime import datetime
 
 
-class WQXweb(object):
+def wqx_monitoring_locations(orgid: str, pk: str, username: str):
+    # handle headers
+    api_endpoint = "https://cdx.epa.gov/WQXWeb/api/MonitoringLocations"
+    params = {"OrganizationIdentifiersCsv": orgid}
+    stamp = datetime.strftime(datetime.utcnow(), "%m/%d/%Y %I:%M:%S %p")
 
-    def __init__(self, username, pk):
-        self.pk = pk
-        self.username = username
+    # prepare the request
+    s = requests.Session()
+    p = requests.Request("GET", api_endpoint, params=params).prepare()
+    uri = p.url
 
-    def make_hedears(self):
-        stamp = datetime.strftime(datetime.utcnow(), "%m/%d/%Y %I:%M:%S %p")
-
-
-    def mon_locations(self, org_id: str):
-        return "you made a call to {}".format(org_id)
-
-    def proj_locations(self, org_id: str):
+    data = "{}{}{}{}".format(username, stamp, uri, "GET")
+    # get the byte representation of the data formatted in utf-8
+    signature = bytes(data, "utf-8")
+    pk_decoded = base64.b64decode(pk)
+    signature_bytes = base64.b64encode(hmac.new(key=pk_decoded, msg=signature, digestmod='sha256').digest())
+    signature_string = signature_bytes.decode('utf-8')
+    headers = {'X-UserID': username, 'X-Stamp': stamp, 'X-Signature': signature_string}
+    p.prepare_headers(headers=headers)
+    r = s.send(p)
+    status_code = r.status_code
+    content = r.content.decode("utf-8")
+    j = json.loads(content)
+    r.close()
+    return {'status_code': status_code, 'content': j}
 
