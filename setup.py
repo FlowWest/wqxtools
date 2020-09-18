7add7ff9f5828a1b97c4f083496372ea3ed865c5
Celestino Salim <celestino.salim@gmail.com>
2020-09-18 19:04:14 -0400

    Updates setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
-import setuptools
+import pathlib
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+HERE = pathlib.Path(__file__).parent
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
 
-setuptools.setup(
+INSTALL_REQUIRES = ["requests"]
+
+setup(
     name="wqxtools",
     version="0.0.1",
     author="Emanuel Rodriguez",
     description="A lightweight interface to the WQX Web Services",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "License :: OSI Approved :: MIT License"
-    ],
-    python_requires=">=3.6"
+    packages=find_packages(),
+    classifiers=["License :: OSI Approved :: MIT License"],
+    python_requires=">=3.6",
+    install_requires=INSTALL_REQUIRES,
 )
