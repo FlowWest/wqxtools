b7876ae0af8b4a70e98a3ea53e1a3360f7d3df69
Celestino Salim <celestino.salim@gmail.com>
2020-09-21 16:36:43 -0400

    updating headers.py and upload.py making them dinamic for get/post

--- wqxtools/headers.py
+++ wqxtools/headers.py
@@ -1,3 +1,4 @@
+import base64
 from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Any
@@ -7,20 +8,18 @@ from typing import Any
 class Headers:
     user_id: str
     cdx_key: str
-    data: Any
-    endpoint: str
+    file_name: str
     timestamp: str = field(default=datetime.strftime(datetime.utcnow(), "%m/%d/%Y %I:%M:%S %p"))
 
     def __post_init__(self):
-        self.endpoint = "{}/{}".format(self.endpoint, self.data.name)
         self.cdx_key = base64.b64decode(self.cdx_key)
 
-    def options(self, method: str = "GET"):
+    def options(self, method: str = "GET", uri: str ) -> dict:
         signature = bytes(
-            "{}{}{}{}".format(self.username, self.timestamp, self.endpoint, method), "utf-8"
+            "{}{}{}{}".format(self.username, self.timestamp, uri, method), "utf-8"
         )
         signature_bytes = base64.b64encode(
-            hmac.new(key=self.endpoint, msg=signature, digestmod="sha256").digest()
+            hmac.new(key=self.cdx_key, msg=signature, digestmod="sha256").digest()
         )
         return {
             "X-UserID": self.user_id,
--- wqxtools/upload.py
+++ wqxtools/upload.py
@@ -1,4 +1,4 @@
-# import requests
+import requests
 import json
 import base64
 import hmac
@@ -7,19 +7,29 @@ from datetime import datetime
 from wqxtools.headers import Headers
 
 
-# class Upload:
-#     # upload_endpoint = "https://cdx.epa.gov/WQXWeb/api/Upload"
-#     def __init__(self, user_id: str, cdx_key: str, data: Any, endpoint: str) -> None:
-#         self.user_id = user_id
-#         self.cdx_key = cdx_key
-#         self.data = data
-#         self.endpoint = endpoint
-#         self.timestamp = datetime.strftime(datetime.utcnow(), "%m/%d/%Y %I:%M:%S %p")
+class CDX:
+    BASE_URL = 'https://cdx.epa.gov/WQXWeb/api/'
+        def __init__(self, user_id: str, cdx_key: str, data: Any) -> None:
+            self.user_id = user_id
+            self.cdx_key = cdx_key
+            self.data = data
+            self.headers = Headers(user_id, cdx_key, data.get("name"))
 
-#     def send(self):
-#         resp = requests.post(url=url, headers=headers, data=file_data)
-#         status_code = resp.status_code
-#         content = json.loads(resp.content.decode("utf-8"))
-#         return {"status_code": status_code, "content": content}
+    def upload(self):
+        uri = BASE_URL + "Upload"
+        resp = requests.post(url=uri, headers=self.headers.options("POST", uri), data=self.data)
+        content = json.loads(resp.content.decode("utf-8"))
+        return {"status_code": resp.status_code, "content": content}
 
-print(Headers())
+    def start_import(self, file_id) -> dict:
+    start_import_endpoint = "https://cdx.epa.gov/WQXWeb/api/StartImport"
+    params = {
+        "importConfigurationId": import_config,
+        "fileId": file_id,
+        "fileType": 'CSV',
+        "newOrExistingData": 1,
+        "uponCompletion": '0',
+        "uponCompletionCondition": '0',
+        "worksheetsToImport": '1',
+        "ignoreFirstRowOfFile": True
+    }
